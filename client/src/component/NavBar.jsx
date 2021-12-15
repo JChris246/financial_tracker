4acb591752f53c588320d852c51362bde6af538c
c0d3rJ <c0d3rJJ@protonmail.com>
2021-12-15 09:49:57 -0400

    Added NavBar and

--- client/src/component/NavBar.jsx
+++ client/src/component/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {MenuIcon, XIcon} from "@heroicons/react/solid";
 
 export const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,6 +18,11 @@ export const NavBar = () => {
             id: 3,
             name: 'History',
             route: '#history'
+        },
+        {
+          id: 4,
+          name: 'Add Transaction',
+          route: '#addtransaction'
         }
     ]
     return (
@@ -62,20 +68,7 @@ export const NavBar = () => {
               className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
               onClick={() => setIsMenuOpen(true)}
             >
-              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                />
-              </svg>
+              <MenuIcon className="w-7 text-gray-300"/>
             </button>
             
             {isMenuOpen && (
@@ -96,12 +89,7 @@ export const NavBar = () => {
                         className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                         onClick={() => setIsMenuOpen(false)}
                       >
-                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                          <path
-                            fill="currentColor"
-                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                          />
-                        </svg>
+                        <XIcon className="w-5 text-gray-300"/>
                       </button>
                     </div>
                   </div>
